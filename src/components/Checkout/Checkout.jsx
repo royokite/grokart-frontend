9087be34f97a8ffa5789023eecb913f993a6a517
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2023-01-16 06:40:25 +0300

    Minor cosmetic changes on checkout page

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -7,7 +7,7 @@ const Checkout = () => {
   return (
     <div className="checkout-container">
     <div className="checkout-card">
-    <h3>Checkout</h3>
+    <h3 className="checkout-title">Checkout</h3>
     <form>
       <div className="card-row">
         <input type="text" id="firstName" placeholder="Name" />
@@ -24,8 +24,9 @@ const Checkout = () => {
         <input type="text" id="card-postal" placeholder="Postal Code" />
         <input type="text" id="card-phone" placeholder="Phone Number" />
       </div>
-      <div className="btn-container">
-        <button type="button" id="nextBtn">Next</button>
+      <div className="btn-container" >
+        <button type= "submit" className="btn btn-primary btn-lg btn-block checkout-btn"
+         id="nextBtn">Next</button>
       </div>
     </form>
   </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { useCart } from "react-use-cart";
 
