7e0fe4b4e51ea710f1a0d278ec054ffd7b03bcf0
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2023-01-17 05:34:48 +0300

    Changed alert from alert to relavant notification and added system styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,37 +1,62 @@
 import React from "react";
 import { useCart } from "react-use-cart";
 import { useState } from "react";
+import { Alert } from "react-bootstrap";
 
 const ProductCard = ({ product }) => {
-    const { addItem, items } = useCart();
-    const [isAdded, setIsAdded] = useState(false);
+  const { addItem, items } = useCart();
+  const [isAdded, setIsAdded] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
-    const addToCart = () =>{
-        addItem(product);
-        console.log(items)
-    };
+  const addToCart = () => {
+    addItem(product);
+    console.log(items);
+  };
 
-      
-        const handleClick = () => {
-          setIsAdded(true);
-          alert('Added to cart!');
-        };
+  const handleClick = () => {
+    setIsAdded(true);
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2000);
+  };
 
-    return (
-            <div className="col-md-3 mb-4 position-relative">
-                <div className="card h-100 text-center p-4 border-0" style={{backgroundColor: "#181a1b"}}>
-                    <img src={product.image} className="card-img-top" alt={product.name} height="250px"/>
-                    <div className="card-body">
-                        <h5 className="card-title mb-0 text-white">{product.name}</h5>
-                        <p className="card-text lead fw-bold" style={{color: "#f97316"}}>
-                            Ksh. {product.price}
-                        </p>
-                        <button className="btn btn-outline-dark position-absolute bottom-0 start-50 translate-middle-x text-white" onClick={() => {addToCart();handleClick();}}>
-                            Add to Cart
-                        </button>
-                    </div>
-                </div>
-            </div>
-    );
+  return (
+    <div className="col-md-3 mb-4 position-relative">
+      <div
+        className="card h-100 text-center p-4 border-0"
+        style={{ backgroundColor: "#181a1b" }}
+      >
+        <img
+          src={product.image}
+          className="card-img-top"
+          alt={product.name}
+          height="250px"
+        />
+        <div className="card-body">
+          <h5 className="card-title mb-0 text-white">{product.name}</h5>
+          <p className="card-text lead fw-bold" style={{ color: "#f97316" }}>
+            Ksh. {product.price}
+          </p>
+          <div>
+            <button
+              className="btn btn-outline-dark position-absolute bottom-0 start-50 translate-middle-x text-white"
+              onClick={() => {
+                addToCart();
+                handleClick();
+              }}
+            >
+              Add to Cart
+            </button>
+            {showAlert && (
+              <Alert variant="success" className="fade-out">
+                Item added to cart!
+              </Alert>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ProductCard;
