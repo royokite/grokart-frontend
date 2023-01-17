bc54dd5fe2324ed4a1033923de9c6c0de8f0cfe0
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2023-01-17 16:11:04 +0300

    Search functionality by name

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,48 +2,114 @@ import React from "react";
 import ProductCard from "./ProductCard";
 import { CartProvider, useCart } from "react-use-cart";
 import Cart from "./cart";
-import './Searchbar/search'
 
-const Products = ({ allProducts, filter, loading, setFilter, filterProduct }) => {
 
- 
+
+const Products = ({
+  allProducts,
+  filter,
+  loading,
+  setFilter,
+  filterProduct,
+}) => {
   const Loading = () => {
-    return(
-     <>
-      Loading....
-    </>    
-    );
-  };  
+    return <>Loading....</>;
+  };
+  const filterByName = () => {
+    const name = document.getElementById("productName").value;
+    const filteredProducts = allProducts.filter(product => product.name.toLowerCase().includes(name.toLowerCase()));
+    setFilter(filteredProducts);
+  }
 
-  const renderProducts= filter.map((product) => 
-    <ProductCard key={product.id} product={product}/>)
-  
+  const renderProducts = filter.map((product) => (
+    <ProductCard key={product.id} product={product} />
+  ));
+
+
+const filterByProduct = (category) => {
+  let filteredProducts;
+  const name = document.getElementById("productName").value;
+  if(category){
+    filteredProducts = allProducts.filter(product => product.category_id === category && product.name.toLowerCase().includes(name.toLowerCase()));
+  }else{
+    filteredProducts = allProducts.filter(product => product.name.toLowerCase().includes(name.toLowerCase()));
+  }
+  setFilter(filteredProducts);
+}
+
   return (
-      <div>
-        <div className="container my-5 py-5 text-white">
-          <div className="row">
-            <div className="col-12 mb-5 color: ">
-              <h1 className="display-6 fw-bolder text-center">Available Products</h1>
-              <hr />
-            </div>
-            
-            <div className="buttons d-flex justify-content-center mb-5">
-              <button className="btn btn-outline-dark me-2" onClick={()=>setFilter(allProducts)}>ALL</button>
-              <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct(1)}>Dairy</button>
-              <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct(2)}>Meat</button>
-              <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct(3)}>Fruits</button>
-              <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct(4)}>Vegetables</button>
-              <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct(5)}>Drinks</button>
-              <button className="btn btn-outline-dark me-2" onClick={()=>filterProduct(6)}>Pantry</button>
-          </div>
-          <hr />
-            <div className="row justify-content-center">
-              {loading ? <Loading /> : renderProducts}
-            </div>
+    <div>
+      <div className="d-flex justify-content-center mb-5">
+        <input
+          className="form-control"
+          type="text"
+          id="productName"
+          placeholder="Enter product name"
+        />
+        <button className="btn btn-outline-dark me-2" onClick={filterByName}>
+          Search
+        </button>
+      </div>
+      <div className="container my-5 py-5 text-white">
+        <div className="row">
+          <div className="col-12 mb-5 color: ">
+            <h1 className="display-6 fw-bolder text-center">
+              Available Products
+            </h1>
             <hr />
           </div>
+          <div className="buttons d-flex justify-content-center mb-5">
+            <button
+              className="btn btn-outline-dark me-2"
+              onClick={() => setFilter(allProducts)}
+            >
+              ALL
+            </button>
+            <button
+              className="btn btn-outline-dark me-2"
+              onClick={() => filterProduct(1)}
+            >
+              Dairy
+            </button>
+            <button
+              className="btn btn-outline-dark me-2"
+              onClick={() => filterProduct(2)}
+            >
+              Meat
+            </button>
+            <button
+              className="btn btn-outline-dark me-2"
+              onClick={() => filterProduct(3)}
+            >
+              Fruits
+            </button>
+            <button
+              className="btn btn-outline-dark me-2"
+              onClick={() => filterProduct(4)}
+            >
+              Vegetables
+            </button>
+            <button
+              className="btn btn-outline-dark me-2"
+              onClick={() => filterProduct(5)}
+            >
+              Drinks
+            </button>
+            <button
+              className="btn btn-outline-dark me-2"
+              onClick={() => filterProduct(6)}
+            >
+              Pantry
+            </button>
+          </div>
+          <hr />
+          <div className="row justify-content-center">
+            {loading ? <Loading /> : renderProducts}
+          </div>
+          <hr />
         </div>
       </div>
+    </div>
   );
 };
 
