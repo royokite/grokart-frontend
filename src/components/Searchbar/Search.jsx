8a952fdbbb403e66528a2204013601cba18e27e2
Lynn W Mwaniki <mwanikilynn8@gmail.com>
2023-01-17 16:38:16 +0300

    search bar,products

--- src/components/Searchbar/Search.jsx
+++ src/components/Searchbar/Search.jsx
@@ -1,62 +1,61 @@
-// import React, { useState } from "react";
-// import "./SearchBar.css";
-// // use react icons
-// import SearchIcon from "@material-ui/icons/Search";
-// import CloseIcon from "@material-ui/icons/Close";
+import React, { useState } from "react";
+import "./SearchBar.css";
+import Products from "../Products";
 
-// function SearchBar({ placeholder, data }) {
-//   const [filteredData, setFilteredData] = useState([]);
-//   const [wordEntered, setWordEntered] = useState("");
 
-//   const handleFilter = (event) => {
-//     const searchWord = event.target.value;
-//     setWordEntered(searchWord);
-//     const newFilter = data.filter((value) => {
-//       return value.title.toLowerCase().includes(searchWord.toLowerCase());
-//     });
+function SearchBar({ placeholder, product }) {
+  const [filteredProduct, setFilteredProduct] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
 
-//     if (searchWord === "") {
-//       setFilteredData([]);
-//     } else {
-//       setFilteredData(newFilter);
-//     }
-//   };
+  const handleFilter = (event) => {
+    const searchProduct = event.target.value;
+    setWordEntered(searchProduct);
+    const newFilter = product.filter((value) => {
+      return value.name.toLowerCase().includes(searchProduct.toLowerCase());
+    });
 
-//   const clearInput = () => {
-//     setFilteredData([]);
-//     setWordEntered("");
-//   };
+    if (searchProduct === "") {
+        setFilteredProduct([]);
+    } else {
+        setFilteredProduct(newFilter);
+    }
+  };
 
-//   return (
-//     <div className="search">
-//       <div className="searchInputs">
-//         <input
-//           type="text"
-//           placeholder={placeholder}
-//           value={wordEntered}
-//           onChange={handleFilter}
-//         />
-//         <div className="searchIcon">
-//           {filteredData.length === 0 ? (
-//             <SearchIcon />
-//           ) : (
-//             <CloseIcon id="clearBtn" onClick={clearInput} />
-//           )}
-//         </div>
-//       </div>
-//       {filteredData.length !== 0 && (
-//         <div className="dataResult">
-//           {filteredData.slice(0, 15).map((value, key) => {
-//             return (
-//               <a className="dataItem" href={value.link} >
-//                 <p>{value.title} </p>
-//               </a>
-//             );
-//           })}
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
+  const clearInput = () => {
+    setFilteredProduct([]);
+    setWordEntered("");
+  };
 
-// export default SearchBar;
+  return (
+    <div className="search">
+      <div className="searchInputs">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handleFilter}
+        />
+        <div className="searchIcon">
+          {filteredProduct.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearBtn" onClick={clearInput} />
+          )}
+        </div>
+      </div>
+      {filteredProduct.length !== 0 && (
+        <div className="dataResult">
+          {filteredProduct.slice(0, 10).map((value, key) => {
+            return (
+              <a className="dataItem" href={value.link} >
+                <p>{value.title} </p>
+              </a>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SearchBar;
