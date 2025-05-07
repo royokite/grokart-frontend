4e2693a6e4a2ce2c681cd6be13a89386b2a59f69
jaime <jamesowiti24@gmail.com>
2025-05-07 19:20:15 +0300

    Added ux for slow speeds

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const Loading = () => {
     return (
-    <div className="justify-content-center text-center">
+      <div className="justify-content-center text-center">
         <div className="spinner-border text-success" role="status">
-            <span className="visually-hidden">Loading...</span>
+          <span className="visually-hidden">Loading...</span>
         </div>
-    </div>
-  );
+        <span className=" text-success">
+          Due to slow speeds of render free tier please hold on while the server
+          spins up
+        </span>
+      </div>
+    );
 };
 
 export default Loading;
